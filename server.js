35ad30c231fd9f0d3cec1823fd5643f0df149793
Eric Carlson <eric.carlson2972@gmail.com>
2017-11-13 21:01:02 -0500

    Setup heroku port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var connect = require('connect');
 var http = require('http');
 var app = connect();
+var port = process.env.PORT || 3000;
 
 // require request-ip and register it as middleware
 var requestIp = require('request-ip');
@@ -13,7 +14,7 @@ app.use(function(req, res) {
 });
 
 //create node.js http server and listen on port
-http.createServer(app).listen(3000);
+http.createServer(app).listen(port);
 
 // test it locally from the command line
 // > curl -X GET localhost:3000 # Hello, your ip address is ::1 and is of type IPv6
